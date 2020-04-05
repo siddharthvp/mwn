19e5cec5e582d571e3d5afcd50ddf06b7c183c3c
Siddharth VP <siddharthvp@gmail.com>
2020-04-05 18:00:37 +0530

    add licence header

--- MWB.js
+++ MWB.js
@@ -1,9 +1,22 @@
 /**
  *
- * MWB bot framework for NodeJS
+ *  MWB: a MediaWiki bot framework for NodeJS
  *
- * Large parts of the code are adapted from MWBot <https://github.com/Fannon/mwbot>
- * which is released under the MIT license. Copyright (c) 2015-2018 Simon Heimler.
+ *  Parts of the code are adapted from MWBot <https://github.com/Fannon/mwbot>
+ *  which is released under the MIT license. Copyright (c) 2015-2018 Simon Heimler.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
