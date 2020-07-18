2250c83adbe14307fe2fcfd119ef43d169c295a3
Siddharth VP <siddharthvp@gmail.com>
2020-07-19 01:32:07 +0530

    tests: use OAuth instead of BotPassword login, as it's faster
    Logging in and getting tokens through botpassword login needs 3 API calls, whereas with OAuth it's just 1.

--- tests/test_base.js
+++ tests/test_base.js
@@ -5,38 +5,36 @@ const log = require('semlog').log;
 const crypto = require('crypto');
 
 const chai = require('chai');
-const expect = chai.expect;
+const expect = chai.expect; 
 const assert = require('assert');
 
-const loginCredentials = require('./mocking/loginCredentials.js').valid;
+const loginCredentials = require('./mocking/loginCredentials.js').valid_oauth;
 
 let bot = new mwn({
 	silent: true,
 	hasApiHighLimit: true,
 	apiUrl: loginCredentials.apiUrl,
-	username: loginCredentials.username,
-	password: loginCredentials.password,
+	OAuthCredentials: loginCredentials.OAuthCredentials,
 	userAgent: 'mwn (https://github.com/siddharthvp/mwn)'
 });
 
 let loginBefore = function() {
 	this.timeout(7000);
-	return bot.login().then(() => {
-		return bot.getTokens();
-	}, err => {
-		log('[E] Failed to log in: ');
-		console.log(err.response);
-	}).then(() => {
+
+	bot.initOAuth();
+	return bot.getTokensAndSiteInfo().then(() => {
 		expect(bot.csrfToken).to.be.a('string');
+		expect(bot.csrfToken.length).to.be.gt(5);
 		assert(bot.csrfToken.endsWith('+\\'));
 		expect(bot.title.nameIdMap).to.be.a('object');
 		expect(bot.title.legaltitlechars).to.be.a('string');
 		expect(bot.title.nameIdMap).to.include.all.keys('project', 'user');
 	});
+
 };
 
 let logoutAfter = function() {
-	return bot.logout();
+
 };
 
 // Export everything
