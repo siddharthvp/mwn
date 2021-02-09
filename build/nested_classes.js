19a570f2f0595e425816248c106f3ad09c973564
Siddharth VP <siddharthvp@gmail.com>
2021-02-09 23:27:35 +0530

    Remove generated files from git repo
    Remove .js and .d.ts files. The custom with TypeScript libraries is to keep only the TS sources in git repo, and only the JS and .d.ts files to the npm package.

--- build/nested_classes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// The interfaces corresponding to the nested classes appear below.
-// The declarations need to be duplicated here because TypeScript doesn't really support
-// nested classes.
-Object.defineProperty(exports, "__esModule", { value: true });
