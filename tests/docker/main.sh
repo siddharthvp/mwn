cfc60c5233cb42f04d53bad579a5bce7aa0cf94e
Siddharth VP <siddharthvp@gmail.com>
2023-05-25 00:27:18 +0530

    CI: run tests in node 18.x as well, skip 14.x

--- tests/docker/main.sh
+++ tests/docker/main.sh
@@ -1,5 +1,4 @@
-docker-compose up --detach --build
-sleep 15 # need to give some time for the database to start working properly
+docker compose up --detach --build
+sleep 15 # need to give some time for the database to start accepting connections
 
-docker exec docker_mediawiki_1 bash /var/www/html/setup.sh
-
+docker compose exec mediawiki bash /var/www/html/setup.sh
