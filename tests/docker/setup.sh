1ba6d7570573c4e41ec45189b050f2b6a2999fb4
Siddharth VP <siddharthvp@gmail.com>
2025-03-16 01:58:54 +0530

    tests: use run.php for mediawiki maintenance scripts

--- tests/docker/setup.sh
+++ tests/docker/setup.sh
@@ -1,6 +1,6 @@
 mv ./LocalSettings.php /tmp/LocalSettings.php
 
-php maintenance/install.php Wikipedia Wikiuser --pass=wikipassword \
+php maintenance/run.php install Wikipedia Wikiuser --pass=wikipassword \
   --server=http://localhost:8080 \
 	--dbserver=database \
 	--dbtype=mysql \
@@ -15,14 +15,14 @@ php maintenance/install.php Wikipedia Wikiuser --pass=wikipassword \
 
 rm -rf ./LocalSettings.php && mv /tmp/LocalSettings.php ./LocalSettings.php
 
-php maintenance/update.php --quick
+php maintenance/run.php update --quick
 
 # Include every grant in bot password, too bad there isn't an --all-grants option
-php maintenance/createBotPassword.php --appid=bp --grants=basic,blockusers,createaccount,createeditmovepage,delete,editinterface,editmycssjs,editmyoptions,editmywatchlist,editpage,editprotected,editsiteconfig,highvolume,mergehistory,oversight,patrol,privateinfo,protect,rollback,sendemail,uploadeditmovefile,uploadfile,viewdeleted,viewmywatchlist,viewrestrictedlogs Wikiuser 12345678901234567890123456789012
+php maintenance/run.php createBotPassword --appid=bp --grants=basic,blockusers,createaccount,createeditmovepage,delete,editinterface,editmycssjs,editmyoptions,editmywatchlist,editpage,editprotected,editsiteconfig,highvolume,mergehistory,oversight,patrol,privateinfo,protect,rollback,sendemail,uploadeditmovefile,uploadfile,viewdeleted,viewmywatchlist,viewrestrictedlogs Wikiuser 12345678901234567890123456789012
 
 # Create a spare account too
-php maintenance/createAndPromote.php Wikiuser2 wikipassword
+php maintenance/run.php createAndPromote Wikiuser2 wikipassword
 
 # Add bot password
-php maintenance/createBotPassword.php --appid=bp --grants=basic,blockusers,createaccount,createeditmovepage,delete,editinterface,editmycssjs,editmyoptions,editmywatchlist,editpage,editprotected,editsiteconfig,highvolume,mergehistory,oversight,patrol,privateinfo,protect,rollback,sendemail,uploadeditmovefile,uploadfile,viewdeleted,viewmywatchlist,viewrestrictedlogs Wikiuser2 12345678901234567890123456789012
+php maintenance/run.php createBotPassword --appid=bp --grants=basic,blockusers,createaccount,createeditmovepage,delete,editinterface,editmycssjs,editmyoptions,editmywatchlist,editpage,editprotected,editsiteconfig,highvolume,mergehistory,oversight,patrol,privateinfo,protect,rollback,sendemail,uploadeditmovefile,uploadfile,viewdeleted,viewmywatchlist,viewrestrictedlogs Wikiuser2 12345678901234567890123456789012
 
